5f5a970576ade7836b9316fcf686cfc315f078d0
Framzy <fardenramzy97@gmail.com>
2025-11-17 18:34:57 +0700

    setup file and tailwind config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import TryComponent from './tryComponent.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TryComponent />
   </StrictMode>,
 )
---
+++ src/tryComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function tryComponent() {
+    return(
+        <>
+            <h1 className="text-3xl text-center font-bold text-blue-600">Try</h1>
+        </>
+    )
+}
+
+export default tryComponent
