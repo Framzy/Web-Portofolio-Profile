b38c201b0fc127d5c198f530d41cf17194f11cf3
Framzy <fardenramzy97@gmail.com>
2025-11-18 20:51:56 +0700

    add image and create styles

--- src/tryComponent.tsx
+++ src/tryComponent.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function tryComponent() {
     return(
         <>
-            <h1 className="text-3xl text-center font-bold text-blue-600">Try</h1>
+            <h1 className="text-3xl text-center font-bold text-color-">Karina</h1>
+            <img className="mx-auto mt-5" src="/images/karina_1.jpg" alt="karina image" width={150}/>
         </>
     )
 }
