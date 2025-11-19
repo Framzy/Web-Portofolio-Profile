198a3ebfd9e2f6c6e9ce70e0f8ffe46652f9d2a3
Framzy <fardenramzy97@gmail.com>
2025-11-19 18:50:37 +0700

    move navbar into navbar function

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,39 +1,15 @@
-import { Link, Outlet } from "react-router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Outlet } from "react-router";
+import Navbar from "./navbar/Navbar";
 
 function Layout() {
   return (
     <>
-      <nav className="bg-[#0A090F] flex justify-between items-center h-[60px]">
-        <h1 className="text-white font-bold ml-20">FRAMZY.</h1>
-        <ul className="text-white flex  gap-5 font-light text-sm">
-          <li className="text-[#39EEFA]">
-            <Link to="/">ABOUT</Link>
-          </li>
-          <li>
-            <Link to="/">SKILLS</Link>
-          </li>
-          <li>
-            <Link to="/">PROJECTS</Link>
-          </li>
-          <li>
-            <Link to="/">
-              <p>
-                CONNECT <FontAwesomeIcon icon={["fas", "caret-down"]} />
-              </p>
-            </Link>
-          </li>
-        </ul>
-        <button
-          type="button"
-          className="bg-white text-[#0A090F] font-medium h-12 w-60 mr-2 rounded cursor-pointer hover:bg-[#39EEFA] "
-        >
-          DONWLOAD CV
-        </button>
-      </nav>
-      <div>
-        <Outlet />
-      </div>
+      <main>
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="bg-[#0A090F] flex justify-between items-center h-[60px] ">
+        <h1 className="text-white font-bold font-vollkorn ml-20">FRAMZY.</h1>
+        <ul className="text-white flex  gap-5 font-light text-sm">
+          <li className="text-[#39EEFA]">
+            <Link to="/">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/">SKILLS</Link>
+          </li>
+          <li>
+            <Link to="/">PROJECTS</Link>
+          </li>
+          <li>
+            <Link to="/">
+              <p>
+                CONNECT <FontAwesomeIcon icon={["fas", "caret-down"]} />
+              </p>
+            </Link>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="bg-white text-[#0A090F] font-medium h-12 w-60 mr-2 rounded cursor-pointer hover:bg-[#39EEFA] "
+        >
+          DONWLOAD CV
+        </button>
+      </nav>
+    </>
+  );
+}
