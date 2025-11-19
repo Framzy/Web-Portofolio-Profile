4e5d3b15fa623a1b2d84af1652e7455ff68995a1
Framzy <fardenramzy97@gmail.com>
2025-11-19 18:34:20 +0700

    cahneg color template to cyan

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ function Layout() {
       <nav className="bg-[#0A090F] flex justify-between items-center h-[60px]">
         <h1 className="text-white font-bold ml-20">FRAMZY.</h1>
         <ul className="text-white flex  gap-5 font-light text-sm">
-          <li className="text-[#FDE63D]">
+          <li className="text-[#39EEFA]">
             <Link to="/">ABOUT</Link>
           </li>
           <li>
@@ -26,7 +26,7 @@ function Layout() {
         </ul>
         <button
           type="button"
-          className="bg-white text-[#0A090F] font-medium h-12 w-60 mr-2 rounded cursor-pointer hover:bg-[#FDE63D] "
+          className="bg-white text-[#0A090F] font-medium h-12 w-60 mr-2 rounded cursor-pointer hover:bg-[#39EEFA] "
         >
           DONWLOAD CV
         </button>
