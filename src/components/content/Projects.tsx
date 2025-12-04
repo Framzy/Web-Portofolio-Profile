fbc24a677a648739daa708d89ff0fd1ef798b0bf
Framzy <fardenramzy97@gmail.com>
2025-12-04 11:12:06 +0700

    styling pagination

--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import type { Swiper as SwiperType } from "swiper";
+import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -52,7 +53,6 @@ export default function Projects({ sectionRef }: Props) {
 
   return (
     <section ref={sectionRef} className="h-screen mt-10">
-      {" "}
       <div className="skills-title w-8/10 mx-auto flex flex-row justify-between items-end py-5">
         <h1 className="text-3xl font-bold text-[#39EEFA]">
           MY <span className="text-white">PROJECTS</span>
@@ -60,38 +60,53 @@ export default function Projects({ sectionRef }: Props) {
         <p className=" text-white font-thin font-poppins text-[10px]">
           ///////////////// MY_PROJECTS
         </p>
-      </div>{" "}
-      <button onClick={() => swiperRef.current?.slidePrev()}>Prev</button>
-      <div className="w-8/10 mx-auto bg-white">
-        <Swiper
-          modules={[Pagination]}
-          slidesPerView={1}
-          spaceBetween={20}
-          onSwiper={handleOnSwiper}
-        >
-          <SwiperSlide>
-            <div className="bg-red-500 h-40">
-              <h1>agafa</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-blue-500 h-40" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-green-500 h-40" />
-          </SwiperSlide>
-        </Swiper>
+      </div>
+      <div className="w-full flex flex-row items-center justify-center gap-5">
+        <div className="left-slide-content">
+          <button
+            title="prevButton"
+            className="cursor-pointer"
+            onClick={() => swiperRef.current?.slidePrev()}
+          >
+            <FaArrowLeftLong size={20} className="text-white" />
+          </button>
+        </div>
+        <div className="slide-content border border-[#898989] w-8/10 ">
+          <Swiper
+            modules={[Pagination]}
+            slidesPerView={1}
+            spaceBetween={20}
+            onSwiper={handleOnSwiper}
+          >
+            <SwiperSlide>
+              <div className="bg-red-500 h-60">
+                <h1>agafa</h1>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="bg-blue-500 h-40" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="bg-green-500 h-40" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
 
-        <div
-          ref={paginationRef}
-          className="my-4 flex justify-center gap-2 custom-pagination"
-          aria-hidden="true"
-        />
-
-        <div className="flex gap-2">
-          <button onClick={() => swiperRef.current?.slideNext()}>Next</button>
+        <div className="right-slide-content">
+          <button
+            title="nextButton"
+            className="cursor-pointer"
+            onClick={() => swiperRef.current?.slideNext()}
+          >
+            <FaArrowRightLong size={20} className="text-white" />
+          </button>
         </div>
       </div>
+      <div
+        ref={paginationRef}
+        className="my-4 flex justify-center gap-2 custom-pagination"
+        aria-hidden="true"
+      />
     </section>
   );
 }
