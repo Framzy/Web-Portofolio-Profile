c36eb1c9d8ce800ca71d310c2bcefd5bb6b7799e
Framzy <fardenramzy97@gmail.com>
2025-12-07 19:46:33 +0700

    add link to project

--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -10,8 +10,16 @@ import "swiper/css/pagination";
 type Props = { sectionRef: RefObject<HTMLDivElement | null> };
 
 const PROJECTS = [
-  { id: "project_01", title: "Posyandu APP - Thesis" },
-  { id: "project_02", title: "Kedai Takjil" },
+  {
+    id: "project_01",
+    title: "Contact Management",
+    link: "https://contact-management-khaki.vercel.app/",
+  },
+  {
+    id: "project_02",
+    title: "Kedai Takjil",
+    link: "https://kedai-takjil.vercel.app/",
+  },
   { id: "project_03", title: "Posyandu APP - Thesis" },
   { id: "project_04", title: "Kedai Takjil" },
   { id: "project_05", title: "Posyandu APP - Thesis" },
@@ -25,11 +33,32 @@ function chunk<T>(arr: T[], size = 2): T[][] {
 }
 
 // card kecil untuk tiap project (reuseable)
-function ProjectCard({ project }: { project: { id: string; title: string } }) {
+function ProjectCard({
+  project,
+}: {
+  project: { id: string; title: string; link?: string };
+}) {
   return (
     <div className="item-1 flex flex-col gap-2 w-fit ">
       <p className="text-xs ">{project.id}</p>
-      <div className="img-item w-xs h-45 sm:w-md sm:h-55 md:w-lg md:h-70 lg:w-sm lg:h-60 xl:w-md 2xl:w-xl 2xl:h-85 bg-amber-50 border border-white rounded-xl" />
+
+      {project.link ? (
+        <a
+          href={project.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block" // pastikan menjadi blok penuh
+        >
+          <div className="img-item flex items-center justify-center w-xs h-45 sm:w-md sm:h-55 md:w-lg md:h-70 lg:w-sm lg:h-60 xl:w-md 2xl:w-xl 2xl:h-85 bg-[rgba(44,168,180,0.41)] border border-white rounded-xl hover:opacity-90 transition">
+            <p className="text-sm text-white">{project.title}</p>
+          </div>
+        </a>
+      ) : (
+        <div className="img-item w-xs h-45 sm:w-md sm:h-55 md:w-lg md:h-70 lg:w-sm lg:h-60 xl:w-md 2xl:w-xl 2xl:h-85 bg-amber-50 border border-white rounded-xl">
+          {project.title}
+        </div>
+      )}
+
       <h2 className="text-sm">{project.title}</h2>
     </div>
   );
