306d9f9677ea7a475995ee9f9e7ddb979dbeefcc
Framzy <fardenramzy97@gmail.com>
2025-12-07 20:00:06 +0700

    repair import link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./styles/index.css";
-// import "./styles/global.css";
 import Layout from "./components/Layout";
-import ErrorPage from "./pages/error/ErrorPage";
+import ErrorPage from "./pages/error/ErrorPage.tsx";
 const Content = lazy(() => import("./components/Content"));
 
 createRoot(document.getElementById("root")!).render(
