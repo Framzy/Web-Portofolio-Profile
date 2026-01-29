4a4d88d6fbe8cc0b74b94cd989f53f2be572ce1a
framzy <fardenramzy97@gmail.com>
2026-01-29 15:04:04 +0700

    refactor skill section

--- src/features/skills/SkillsSection.tsx
+++ src/features/skills/SkillsSection.tsx
@@ -1,12 +1,12 @@
 import { forwardRef } from "react";
-import { SKILLS } from "./data/SkillsItems.data";
+import { SKILLS_ITEMS } from "./data/SkillsItems.data";
 import SkillCard from "./components/SkillCard";
 
 const SkillsSection = forwardRef<HTMLDivElement>(
   function SkillsSection(_, ref) {
     return (
       <>
-        <section ref={ref} className="h-screen mt-10">
+        <section ref={ref} className="skills-content min-h-screen">
           <div className="skills-title w-8/10 mx-auto flex flex-row justify-between items-end py-5">
             <h1 className="text-3xl  font-bold text-[#39EEFA]">SKILLS</h1>
             <p className=" text-neutral-300 font-thin font-poppins text-[10px]">
@@ -15,7 +15,7 @@ const SkillsSection = forwardRef<HTMLDivElement>(
           </div>
 
           <div className="text-white text-sm w-8/10 mx-auto grid grid-cols-2 gap-5 font-manrope">
-            {SKILLS.map((skill, index) => (
+            {SKILLS_ITEMS.map((skill, index) => (
               <SkillCard key={skill.label} index={index} {...skill} />
             ))}
           </div>
--- src/features/skills/data/SkillsItems.data.ts
+++ src/features/skills/data/SkillsItems.data.ts
@@ -1,20 +1,21 @@
-export const SKILLS = [
-  {
-    label: "Programming languages",
-    span: 2,
-    items: ["JavaScript", "TypeScript", "Java", "PHP", "Python"],
-  },
-  {
-    label: "Frameworks & Libraries",
-    span: 2,
-    items: ["React", "Tailwind", "Bootstrap"],
-  },
-  { label: "Databases", span: 2, items: ["MySQL", "PostgreSQL"] },
-  {
-    label: "Tools, Softwares, And Others",
-    span: 2,
-    items: ["VS Code", "Git", "GitHub"],
-  },
-  { label: "Operating Systems", span: 1, items: ["Windows", "Mac"] },
-  { label: "Cloud", span: 1, items: ["Vercel", "Supabase"] },
-];
+export const SKILLS_ITEMS: { label: string; span: number; items: string[] }[] =
+  [
+    {
+      label: "Programming languages",
+      span: 2,
+      items: ["JavaScript", "TypeScript", "Java", "PHP", "Python"],
+    },
+    {
+      label: "Frameworks & Libraries",
+      span: 2,
+      items: ["React", "Tailwind", "Bootstrap"],
+    },
+    { label: "Databases", span: 2, items: ["MySQL", "PostgreSQL"] },
+    {
+      label: "Tools, Softwares, And Others",
+      span: 2,
+      items: ["VS Code", "Git", "GitHub"],
+    },
+    { label: "Operating Systems", span: 1, items: ["Windows", "Mac"] },
+    { label: "Cloud", span: 1, items: ["Vercel", "Supabase"] },
+  ];
