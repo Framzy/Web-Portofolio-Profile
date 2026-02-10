f4b8788100ef769b3e25ff52a7e7f8a26c5e9bb9
framzy <fardenramzy97@gmail.com>
2026-02-10 23:57:43 +0700

    adjust some style

--- src/features/projects/layouts/ProjectGrid.tsx
+++ src/features/projects/layouts/ProjectGrid.tsx
@@ -9,9 +9,14 @@ export default function ProjectsGrid({ projects }: ProjectsGridProps) {
   if (!projects.length) return null;
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-2 ">
       {projects.map((project) => (
-        <ProjectCard key={project.id} project={project} />
+        <div
+          key={project.id}
+          className="border border-white px-6 py-4 text-white"
+        >
+          <ProjectCard key={project.id} project={project} />
+        </div>
       ))}
     </div>
   );
