02b84357677bde91033e42aec586a37a4d3f1734
framzy <fardenramzy97@gmail.com>
2026-02-13 17:01:17 +0700

    finishing project grid from project layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <>
-      <div className="h-20 flex border-t border-[#8A8A8A] items-center justify-center">
+      <div className="h-20 flex border-t border-[#8A8A8A] items-center justify-center mt-10">
         <p className="text-white text-center font-manrope text-sm">
-          © 2025 Framzy. All rights reserved
+          ©{year} Framzy. All rights reserved
         </p>
       </div>
     </>
--- src/features/projects/layouts/ProjectGrid.tsx
+++ src/features/projects/layouts/ProjectGrid.tsx
@@ -9,13 +9,13 @@ export default function ProjectsGrid({ projects }: ProjectsGridProps) {
   if (!projects.length) return null;
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 ">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
       {projects.map((project, index) => (
         <div
           key={project.id}
           className="border border-white px-6 py-4 text-white"
         >
-          <ProjectCard key={project.id} project={project} number={index + 1} />
+          <ProjectCard project={project} number={index + 1} />
         </div>
       ))}
     </div>
