bd1500f889f5fef42ec65d0f74c97adb1b93657e
framzy <fardenramzy97@gmail.com>
2026-07-09 16:30:59 +0700

    Update style of about top

--- src/features/about/components/ui/PersonalDescription.tsx
+++ src/features/about/components/ui/PersonalDescription.tsx
@@ -2,6 +2,8 @@ import { Personal_Description } from "../../data/AboutItems.data";
 
 export default function PersonalDescription() {
   return (
-    <p className="pt-10 text-white text-justify">{Personal_Description}</p>
+    <p className="pt-10 text-white text-justify text-sm md:text-base">
+      {Personal_Description}
+    </p>
   );
 }
--- src/features/about/layouts/AboutTop.tsx
+++ src/features/about/layouts/AboutTop.tsx
@@ -11,28 +11,32 @@ export default function AboutTop({ onNavigate }: Props) {
 
   return (
     <>
-      <div className="flex flex-col w-4/5 lg:w-fit mx-auto box-border p-4">
-        <div className="flex items-center justify-center font-manrope font-bold text-6xl box-border">
-          <p className="text-[#39EEFA]  md:text-center tracking-wide">
-            SOFTWARE <span className="text-white">ENGINEER</span>
-          </p>
+      <div className="flex w-4/5 md:w-fit mx-auto box-border p-4 md:p-8">
+        <div className="w-full flex flex-col items-center justify-center box-border gap-2">
+          <div className="w-full flex items-center justify-center md:justify-start box-border">
+            <p className="text-[#39EEFA] font-manrope font-bold text-4xl md:text-6xl tracking-wide">
+              SOFTWARE <span className="text-white">ENGINEER</span>
+            </p>
+          </div>
+          <div className="w-full flex items-center justify-center md:justify-start box-border">
+            <p className="text-white font-manrope text-xs md:text-sm">
+              BASED IN JAKARTA
+            </p>
+          </div>
         </div>
-        <p className="text-white font-manrope text-sm text-start md:text-center lg:text-start">
-          BASED IN JAKARTA
-        </p>
       </div>
 
-      <div className="flex flex-col w-5/6 xl:w-4/6 mx-auto box-border ">
-        <div className="flex flex-col md:flex-row justify-center gap-10 box-border">
-          <div className="w-fit max-w-90 mx-auto">
+      <div className="flex flex-col w-5/6 xl:w-4/6 mx-auto box-border">
+        <div className="flex flex-col md:flex-row justify-center box-border gap-6 md:gap-0">
+          <div className="w-4/5 md:w-2/5 xl:w-fit max-w-90 mx-auto pt-5">
             <ProfileImage />
           </div>
 
-          <div className="flex flex-col w-4/5 md:w-3/7 mx-auto pt-5 ">
+          <div className="flex flex-col w-4/5 md:w-3/7 mx-auto ">
             <p className="aboutMe-line text-neutral-300 font-thin font-poppins text-end text-[10px] ">
               ///////////////// About_Me
             </p>
-            <p className="aboutMe-name-line text-neutral-400 flex flex-col font-manrope text-3xl">
+            <p className="aboutMe-name-line text-neutral-400 flex flex-col font-manrope text-2xl md:text-3xl">
               HELLO
               <span className="text-white">I’M FARDEN RAMZY MUHARRAM</span>
             </p>
