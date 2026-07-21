d0f9b8747c8287e12944644eabbb3468b1b616fc
framzy <fardenramzy97@gmail.com>
2026-07-21 10:11:37 +0700

    rename motion variant to ts for fixing the issue on typescript and also adding as const on every const

--- src/utils/motionVariants.ts
+++ src/utils/motionVariants.ts
@@ -3,33 +3,34 @@ export const fadeInLeft = {
   whileInView: { opacity: 1, x: 0 },
   transition: { duration: 0.8, ease: "easeOut" },
   viewport: { once: true, amount: 0.3 },
-};
+} as const;
 
 export const fadeInRight = {
   initial: { opacity: 0, x: -100 },
   whileInView: { opacity: 1, x: 0 },
   transition: { duration: 0.8, ease: "easeOut" },
   viewport: { once: true, amount: 0.3 },
-};
+} as const;
 export const fadeInTop = {
   initial: { opacity: 0, y: 100 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.8, ease: "easeOut" },
   viewport: { once: true, amount: 0.3 },
-};
+} as const;
+
 export const fadeInBottom = {
   initial: { opacity: 0, y: -100 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.8, ease: "easeOut" },
   viewport: { once: true, amount: 0.3 },
-};
+} as const;
 
 export const fadeIn = {
   initial: { opacity: 0 },
   whileInView: { opacity: 1 },
   transition: { duration: 0.8, ease: "easeIn" },
   viewport: { once: true, amount: 0.3 },
-};
+} as const;
 
 export const fadePopUp = {
   initial: {
@@ -45,4 +46,4 @@ export const fadePopUp = {
     opacity: 0,
   },
   transition: { duration: 0.2, ease: "easeInOut" },
-};
+} as const;
